7479e1464774c112e7f10aa266eb399054bde255
Sabina Maria Borbély <32135827+sabinamp@users.noreply.github.com>
2021-06-21 17:12:56 +0200

    add apiURL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9792e4ea.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
